290b9f503550d191767442d240729d9c5e086d0a
yuanfeng <yuanfeng@adtech.com.cn>
2019-11-17 10:56:59 +0800

    变量部分小demo

---
+++ GoDemo/src/go_code/godemo191117/homework1.go
@@ -0,0 +1,45 @@
+package main
+
+////演示全局变量
+//var n1 = 100
+//var n2 = 200
+//var name = "jack"
+//
+//
+////全局变量简写
+//var(
+//	n3 = 400
+//	n4 = 500
+//	n5 = 600
+//)
+func main() {
+	////打印籍贯信息
+	////fmt.Println("姓名\t性别\t籍贯\t住址\ntom\t\t男\t\t重庆\t长寿区")
+	//
+	////声明不赋值
+	////int默认值就是0
+	//var i int
+	//fmt.Println("i=",i)
+	//
+	////根据值自行判定变量类型（类型推导）
+	//var num = 11.11
+	//fmt.Println("num=",num)
+	//
+	////省略var 的时候使用:=
+	////下面的方式等价于var name string name = "tom"
+	//name :="zhangsan"
+	//fmt.Println("name = ",name)
+	//
+	////多变量赋值,变量类型一样
+	//var n1,n2,n3 int
+	//fmt.Println("n1=",n1,"n2=",n2,"n3=",n3)
+
+	////多变量赋值,变量类型不一样
+	//var n1,name,n3 = 100,"zhangsan",200
+	//fmt.Println(n1,name,n3)
+
+	////输出全局变量
+	//fmt.Println("n1=",n1,"n2=",n2,"name=",name)
+	//fmt.Println("n3=",n3,"n4=",n4,"n5=",n5)
+
+}
